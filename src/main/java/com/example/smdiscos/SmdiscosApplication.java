972c58375d5cacaf387bc6efd989d9ac31d28262
Felipe Evangelista <satoshikun7@gmail.com>
2022-05-16 21:49:15 -0300

    Smdiscos

---
+++ src/main/java/com/example/smdiscos/SmdiscosApplication.java
@@ -0,0 +1,13 @@
+package com.example.smdiscos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmdiscosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmdiscosApplication.class, args);
+	}
+
+}
