77e31c923afbcc16185b3cba38cb09af1ddc7097
priscilasapaiva <105684748+priscilasapaiva@users.noreply.github.com>
2022-05-21 16:32:11 -0300

    Add files via upload
    Classes

---
+++ src/main/smdiscos/administrador/modelo/Administrador.java
@@ -0,0 +1,22 @@
+package smdiscos.administrador.modelo;
+
+import java.io.Serializable;
+
+/**
+ * Classe que representa a entidade administrador
+ */
+public class Admin implements Serializable {
+    
+    private Integer id;
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    
+}
